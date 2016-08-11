e4aba3c323cbe8cc0e32e26bae10440a4dccacf2
unknown <yukbrandes@gmail.com>
2016-08-11 17:48:17 +0800

    add introduction for decision tree 20160811

--- Decision Tree/creditDecisionTree.R
+++ Decision Tree/creditDecisionTree.R
@@ -7,9 +7,22 @@ credit$dependents <- as.factor(credit$dependents)
 summary(credit)
 str(credit)
 
-
-# deal data
+# prepare data
+summary(credit$months_loan_duration)
+factor_Var <- function(x){
+  temp <- x
+  q1 <- quantile(x,0.25)
+  q2 <- quantile(x,0.5)
+  q3 <- quantile(x,0.75)
+  x[temp <= q1] <- 'A'
+  x[temp >q1 & temp <= q2] <- 'B'
+  x[temp >q2 & temp <= q3] <- 'C'
+  x[temp >q3] <- 'D'
+  x <- as.factor(x)
+  return(x)
+}
 
 # decision tree model
+
 
 # evluate model
